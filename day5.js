27d97f4284b8438ae8c47472136f1c3006250119
Caio Alonso <caio@caioalonso.com>
2018-12-10 18:39:07 -0200

    Day 5 part 2

--- day5.js
+++ day5.js
@@ -15,29 +15,45 @@ function swapCase(letters) {
   return newLetters;
 }
 
-let inverted = swapCase(input);
-input = input.split("");
-inverted = inverted.split("");
+function reactPolymer(p) {
+  let inverted = swapCase(p);
+  p = p.split("");
+  inverted = inverted.split("");
 
-let found = true;
-while (found === true) {
-  found = false;
-  let toRemove = [];
-  // < input.length-1 because we want to skip the last one
-  for (let i = 0; i < input.length - 1; i++) {
-    if (input[i] === inverted[i + 1]) {
-      toRemove.push(i);
-      toRemove.push(i + 1);
-      i++;
-      found = true;
+  let found = true;
+  while (found === true) {
+    found = false;
+    let toRemove = [];
+    // < input.length-1 because we want to skip the last one
+    for (let i = 0; i < p.length - 1; i++) {
+      if (p[i] === inverted[i + 1]) {
+        toRemove.push(i);
+        toRemove.push(i + 1);
+        i++;
+        found = true;
+      }
+    }
+
+    // removing in reverse order to do it in one pass
+    for (let i = toRemove.length - 1; i >= 0; i--) {
+        p.splice(toRemove[i], 1);
+      inverted.splice(toRemove[i], 1);
     }
   }
-
-  // removing in reverse order to do it in one pass
-  for (let i = toRemove.length - 1; i >= 0; i--) {
-    input.splice(toRemove[i], 1);
-    inverted.splice(toRemove[i], 1);
-  }
+  return p.join('');
 }
 
-console.log(input.length)
+let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+let s = alphabet.length
+let min = {letter:'', size:99999}
+while(s--) {
+    let regex = new RegExp(alphabet.charAt(s), 'gmi')
+    let newInput = input.replace(regex, '')
+    let size = reactPolymer(newInput).length;
+    if(size < min.size) {
+        min.letter = alphabet.charAt(s)
+        min.size = size
+    }
+}
+
+console.log(min)
